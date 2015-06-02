cdc792f12adeb48a6b7ba09004db7aa542d4ba24
Olivia <Olivia@Olivias-MacBook-Air.local>
2015-06-01 21:47:53 -0400

    Initial Commit

---
+++ WakeMeUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WakeMeUp
+//
+//  Created by Olivia on 2015-06-01.
+//  Copyright (c) 2015 Olivia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
