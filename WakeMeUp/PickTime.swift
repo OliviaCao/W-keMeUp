61f413c2e45c4c491e85aed33c621989dd84c456
Olivia <Olivia@Olivias-MacBook-Air.local>
2015-06-16 21:37:00 -0400

    Took out Navigation Editor

---
+++ WakeMeUp/PickTime.swift
@@ -0,0 +1,37 @@
+//
+//  PickTime.swift
+//  WakeMeUp
+//
+//  Created by Olivia on 2015-06-15.
+//  Copyright (c) 2015 Olivia. All rights reserved.
+//
+
+import UIKit
+
+class PickTime: UIViewController {
+
+    @IBOutlet weak var DatePickerObject: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
